708133d3dc25f599f4c3ecc4f5a613afdaf67d6b
Aron Raphael Pogere <pogere.aron@gmail.com>
2026-06-05 00:23:51 -0300

    feat: adiciona validacao de duplicidade

--- src/models/CatalogoPokemon.ts
+++ src/models/CatalogoPokemon.ts
@@ -2,4 +2,17 @@ import { PokemonResumo } from './Pokemon';
 
 export class CatalogoPokemon {
   private pokemons: PokemonResumo[] = [];
+
+  adicionar(pokemon: PokemonResumo): void {
+    const jaExiste = this.pokemons.some((item) => item.id === pokemon.id);
+
+    if (jaExiste) {
+      console.log(`[AVISO] ${pokemon.nome} já está no catálogo.`);
+      return;
+    }
+
+    this.pokemons.push(pokemon);
+
+    console.log(`[OK] ${pokemon.nome} adicionado ao catálogo.`);
+  }
 }
